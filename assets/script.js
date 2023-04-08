70f0d5f59bab7bbd31b3c22cb2628da83de7a2e6
Sharia PONN <shariaponn@gmail.com>
2023-04-08 12:28:02 +0200

    first commit

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,39 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+const tagline = document.getElementById("tagline");
+const slideImg = document.getElementById("slide");
+const dots = document.querySelectorAll(".dots .dot");
+
+let numero = 0;
+
+function ChangeSlide(sens) {
+    numero = numero + sens;
+    if (numero < 0)
+        numero = slides.length - 1;
+    if (numero > slides.length)
+        numero = 0;
+    slideImg.src = "./assets/images/slideshow/" + slides[numero].image;
+    tagline.innerHTML = slides[numero].tagLine;
+
+    dots.forEach((dot, index) => {
+        if (index === numero) {
+            dot.classList.add("dot_selected");
+        } else {
+            dot.classList.remove("dot_selected");
+        }
+    });
+}
+
+const button = document.querySelectorAll('.arrow');
+
+button.forEach(clickArrow);
+function clickArrow(element) {
+
+	element.addEventListener('click', (event) => {
+		event.target.parentElement.classList.contains('arrow_left') ? ChangeSlide(-1) : ChangeSlide(1);
+	});
+}
+
+setInterval("ChangeSlide(1)", 3000);
